926910510ebc0d6d0a9e47913dee3796bcaed086
frances-hansen <francesjuniperhansen@users.noreply.github.com>
2020-07-21 21:42:08 -0700

    moving_zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -1,11 +1,46 @@
+# next time this task should be way more clear. "moving zeros" could mean 100 different things.
+# should be "moving zeros to the end of the list". come on now!!!!
+
 '''
 Input: a List of integers
 Returns: a List of integers
-'''
-def moving_zeroes(arr):
-    # Your code here
 
-    pass
+'''
+
+def moving_zeroes(arr):
+    new_list = []
+    pop_count=+1
+
+    for x in range(len(arr)):
+        if arr[x] == 0:
+            y = arr[x]
+            arr.remove(arr[x])
+            arr.insert((len(arr)+1), y)
+    return arr
+
+
+##def moving_zeroes(arr):
+    # Your code here
+##    new_list = []
+##
+##    for x in arr:
+##        if x != 0 and x not in new_list:
+##            new_list.append(x)
+##    return new_list
+
+
+# for [3, 1]:
+##def moving_zeroes(arr):
+##    new_list = []
+##    pop_count = 0
+##
+##    for x in arr:
+##        if arr[x-pop_count] and x not in new_list:
+##            new_list.append(arr.pop(x-pop_count))
+##            pop_count=+1
+##    return new_list    
+
+    
 
 
 if __name__ == '__main__':
